f28c52b54d1bf815992eb3f17d320c145434a752
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2024-05-24 16:55:17 -0300

    update

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,11 @@
 const db = require('mysql2')
+const dotenv = require('dotenv').config()
 
 const conn = db.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'system2024',
-  database: 'basico' 
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 })
 
 conn.connect(function(err){
