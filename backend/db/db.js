f1f421ae5a985823c1ee6422a7fa400d9110b504
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2024-05-25 15:37:56 -0300

    update

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,7 +5,8 @@ const conn = db.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
+  uri: process.env.URI 
 
 })
 
