79d866cb4a214fb9ef9f755896d02f0bedb37de7
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2024-05-26 11:28:33 -0300

    update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express()
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const QuantAdmin = require('./Routes/QuantAdmin');
-const QuantDes = require('./Routes/QuantDes')
+const QuantDes = require('./Routes/QuantDes');
 const QuantUsers = require('./Routes/QuantUsers');
 const SumSalary = require('./Routes/SumSalary');
 const SumSalaryAdm = require('./Routes/SumSalaryAdm');
@@ -12,11 +12,23 @@ const Logout = require('./Routes/Logout');
 const verifyRouter = require('./Routes/VerifyUsers');
 const QueryUsers = require('./Routes/QueryUsers');
 const QueryCategory = require('./Routes/QueryCategory')
+const QueryEmails = require('./Routes/QueryEmails');
+const UpdateUsers = require('./Routes/UpdateUsers');
+const RegisterUsers = require('./Routes/RegisterUsers');
+const RegisterCategory = require('./Routes/RegisterCategory');
+const RegisterEmail = require('./Routes/RegisterEmail');
+const DeleteUser = require('./Routes/DeleteUser');
+const DeleteEmail = require('./Routes/DeleteEmail');
+const sendGmail = require('./Routes/sendGmail');
+const sendOutlook = require('./Routes/sendOutlook');
+const whats = require('./Routes/Whats.js');
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Servidor ok!')
 })
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({origin: ["https://sistemagestaousuarios.onrender.com"], 
 methods: ["GET","POST", "PUT", "DELETE"],
@@ -26,6 +38,9 @@ credentials: true
 app.use(cookieParser());
 app.use(express.json());
 
+app.use('/auth', RegisterUsers);
+app.use('/auth', RegisterCategory);
+app.use('/auth', RegisterEmail);
 app.use('/auth', QuantAdmin);
 app.use('/auth', QuantDes);
 app.use('/auth', QuantUsers);
@@ -36,10 +51,16 @@ app.use('/auth', Logout);
 app.use('/auth', verifyRouter);
 app.use('/auth', QueryUsers);
 app.use('/auth', QueryCategory);
+app.use('/auth', QueryEmails);
+app.use('/auth', UpdateUsers);
+app.use('/auth', DeleteUser);
+app.use('/auth', DeleteEmail);
+app.use('/auth', sendGmail);
+app.use('/auth', sendOutlook);
+app.use('/auth', whats);
 
 
 app.use(express.static('Public'));
-
 
 
 app.listen(port, () => {
